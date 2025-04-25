2dd68559de9dc60d002badea91e54983aa552604
Guilherme Vieira Rosa <guilhermevieirarosa@gmail.com>
2025-04-25 19:30:20 -0300

    version 2

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 #define I2C_SCL 22 // ESP32 SCL on GPIO22
 
 #define UPPER_BOUND  0X4000  // max readout capacitance
-#define LOWER_BOUND  (-1 * UPPER_BOUND)
+#define LOWER_BOUND  0x2000  //(-1 * UPPER_BOUND)
 #define CHANNEL 0
 #define MEASURMENT 0
 
@@ -31,15 +31,34 @@ void loop()
   {
     lastMillis += interval;
 
+    //Serial.printf("---------------------------------------------------------------\r\n", 0);
     uint16_t value[2];
     if (!FDC.readMeasurement(MEASURMENT, value))
     {
-      int16_t msb = (int16_t) value[0];
+      uint16_t msb = (int16_t) value[0];
+      uint16_t lsb = (int16_t) value[1];
+      //Serial.printf("msb: 0x%04X\n", msb);
+      //Serial.printf("lsb: 0x%04X\n", lsb);
+
+      uint32_t byte = (uint32_t) (msb<<16 | lsb);
+      //Serial.printf("byte: 0x%08X\n", read);
+      byte = byte & 0xFFFFFF00;
+      byte = byte>>8;
+      //Serial.printf("byte: 0x%08X\n", byte);
+
+      //Serial.printf("capdac: %d\n", capdac);
+      float offset = ((float) (capdac))*3.125;
+      Serial.printf("offset: %7.3f pF || ", offset);
+      float cap = ((float) ((float) byte / 0x00080000));
+      Serial.printf("capac:  %7.3f pF || ", cap);
+      Serial.printf("total:  %7.3f pF || ", cap + offset);
+
       int32_t capacitance = ((int32_t)457) * ((int32_t)msb); // in attofarads
       capacitance /= 1000;   // in femtofarads
       capacitance += ((int32_t)3028) * ((int32_t)capdac);
 
       Serial.println((((float)capacitance / 1000)), 4);
+      
 
       // Adjust CAPDAC if needed
       if (msb > UPPER_BOUND)
